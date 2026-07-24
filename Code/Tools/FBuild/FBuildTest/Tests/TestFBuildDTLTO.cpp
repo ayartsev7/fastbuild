71786371a60ae00fc722e80aee82b4a7faf85342
Anton Yartsev <ayartsev@accesssoftek.com>
2026-07-25 01:41:42 +0300

    InitializeFromDTLTO() covered

--- Code/Tools/FBuild/FBuildTest/Tests/TestFBuildDTLTO.cpp
+++ Code/Tools/FBuild/FBuildTest/Tests/TestFBuildDTLTO.cpp
@@ -9,6 +9,8 @@
 //------------------------------------------------------------------------------
 TEST_GROUP( TestFBuildDTLTO, FBuildTest )
 {
+protected:
+    void MakeValidDTLTOJson( const char * jsonFile ) const;
 };
 
 //------------------------------------------------------------------------------
@@ -17,6 +19,70 @@ TEST_CASE( TestFBuildDTLTO, InitializeFromDTLTO )
     const char * testDir = "../tmp/Test/FBuildDTLTO/";
     const char * jsonFile = "../tmp/Test/FBuildDTLTO/dist-file.json";
     EnsureDirExists( testDir );
+    MakeValidDTLTOJson( jsonFile );
+
+    FBuildTestOptions options;
+    options.m_DTLTOFile = jsonFile;
+    options.m_SaveDBOnCompletion = true;
+
+    FBuildForTest fBuild( options );
+    TEST_ASSERT( fBuild.Initialize() );
+
+    TEST_ASSERT( fBuild.GetNode( "all" ) );
+    TEST_ASSERT( fBuild.GetOptions().m_SaveDBOnCompletion == false );
+}
+
+//------------------------------------------------------------------------------
+TEST_CASE( TestFBuildDTLTO, MissingDTLTOFile )
+{
+    const char * jsonFile = "../tmp/Test/FBuildDTLTO/missing-dist-file.json";
+    EnsureFileDoesNotExist( jsonFile );
+
+    FBuildTestOptions options;
+    options.m_DTLTOFile = jsonFile;
+
+    FBuildForTest fBuild( options );
+    TEST_ASSERT( fBuild.Initialize() == false );
+    TEST_ASSERT( GetRecordedOutput().Find( "DTLTO: failed to open" ) );
+}
+
+//------------------------------------------------------------------------------
+TEST_CASE( TestFBuildDTLTO, MalformedDTLTOJson )
+{
+    const char * testDir = "../tmp/Test/FBuildDTLTO/";
+    const char * jsonFile = "../tmp/Test/FBuildDTLTO/malformed-dist-file.json";
+    EnsureDirExists( testDir );
+    MakeFile( jsonFile, "not json" );
+
+    FBuildTestOptions options;
+    options.m_DTLTOFile = jsonFile;
+
+    FBuildForTest fBuild( options );
+    TEST_ASSERT( fBuild.Initialize() == false );
+    TEST_ASSERT( GetRecordedOutput().Find( "DTLTO: expected '{'" ) );
+}
+
+//------------------------------------------------------------------------------
+TEST_CASE( TestFBuildDTLTO, NonDefaultTargetWarns )
+{
+    const char * testDir = "../tmp/Test/FBuildDTLTO/";
+    const char * jsonFile = "../tmp/Test/FBuildDTLTO/non-default-target-dist-file.json";
+    EnsureDirExists( testDir );
+    MakeValidDTLTOJson( jsonFile );
+
+    FBuildTestOptions options;
+    options.m_DTLTOFile = jsonFile;
+    options.m_Targets.EmplaceBack( "target" );
+
+    FBuildForTest fBuild( options );
+    TEST_ASSERT( fBuild.Initialize() );
+    TEST_ASSERT( GetRecordedOutput().Find( "Only default target 'all' is supported in DTLTO mode" ) );
+}
+
+// MakeValidDTLTOJson
+//------------------------------------------------------------------------------
+void TestFBuildDTLTO::MakeValidDTLTOJson( const char * jsonFile ) const
+{
     MakeFile( jsonFile, R"({
         "common": {
             "linker_output": "app.exe",
@@ -31,19 +97,6 @@ TEST_CASE( TestFBuildDTLTO, InitializeFromDTLTO )
             }
         ]
     })" );
-
-    FBuildTestOptions options;
-    options.m_DTLTOFile = jsonFile;
-    options.m_SaveDBOnCompletion = true;
-
-    FBuildForTest fBuild( options );
-    TEST_ASSERT( fBuild.Initialize() );
-
-    const Node * all = fBuild.GetNode( "all" );
-    TEST_ASSERT( all );
-    TEST_ASSERT( all->GetType() == Node::ALIAS_NODE );
-    TEST_ASSERT( all->GetStaticDependencies().GetSize() == 1 );
-    TEST_ASSERT( fBuild.GetOptions().m_SaveDBOnCompletion == false );
 }
 
 //------------------------------------------------------------------------------
